748f52d50969c2ce5b89ce331a56ae1a9d2cf8ea
AlvaroR15 <alvaroramos048@gmail.com>
2024-05-20 20:02:02 -0300

    Primeros 3 puntos del tp terminados

---
+++ src/main/java/ar/edu/unju/fi/model/Carrera.java
@@ -0,0 +1,59 @@
+package ar.edu.unju.fi.model;
+
+public class Carrera {
+	private Integer codigo;
+	private String nombre;
+	private Byte cantidadDeAnios;
+	private String estado;
+	
+	public Carrera() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Carrera(Integer codigo, String nombre, Byte cantidadDeAnios, String estado) {
+		this.codigo = codigo;
+		this.nombre = nombre;
+		this.cantidadDeAnios = cantidadDeAnios;
+		this.estado = estado;
+	}
+
+	public Integer getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(Integer codigo) {
+		this.codigo = codigo;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public Byte getCantidadDeAnios() {
+		return cantidadDeAnios;
+	}
+
+	public void setCantidadDeAnios(Byte cantidadDeAnios) {
+		this.cantidadDeAnios = cantidadDeAnios;
+	}
+
+	public String getEstado() {
+		return estado;
+	}
+
+	public void setEstado(String estado) {
+		this.estado = estado;
+	}
+
+	@Override
+	public String toString() {
+		return "Carrera [codigo=" + codigo + ", nombre=" + nombre + ", cantidadDeAnios=" + cantidadDeAnios + ", estado="
+				+ estado + "]";
+	}
+	
+	
+}
---
+++ src/main/java/ar/edu/unju/fi/model/Materia.java
@@ -0,0 +1,11 @@
+package ar.edu.unju.fi.model;
+
+public class Materia {
+	private String codigo;
+	private String nobre;
+	private String curso;
+	private float cantidadHoras;
+	private String modalidad;
+	private Docente docente;
+	private Carrera carrera;
+}
