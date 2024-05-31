3efc6539269f837557309aa74c9e6667fa1a3d32
AlvaroR15 <alvaroramos048@gmail.com>
2024-05-31 00:14:29 -0300

    Uso de Autowired en CollectionMateria

--- src/main/java/ar/edu/unju/fi/collections/CollectionMateria.java
+++ src/main/java/ar/edu/unju/fi/collections/CollectionMateria.java
@@ -6,6 +6,8 @@ import java.util.List;
 import java.util.Optional;
 import java.util.function.Predicate;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 import ar.edu.unju.fi.model.Carrera;
 import ar.edu.unju.fi.model.Docente;
 import ar.edu.unju.fi.model.Materia;
@@ -22,23 +24,20 @@ public class CollectionMateria {
 	
 	
 */
+	
+	@Autowired
+	public CollectionCarrera collectionCarrera;
+	public CollectionDocente collectionDocente;
 
 	private static List<Materia> materias = new ArrayList<Materia>();
 	
 	
 	public List<Materia> getMaterias() {
 		if (materias.isEmpty()) {
-			Docente docente1 = new Docente("111","Marcelo","Ibarra","perez10@gmail.com","388602932");
-			Docente docente2 = new Docente("222","Maria","Cornell","mssCornell@gmail.com","388606955");
-			Docente docente3 = new Docente("333","Ariel","Vega","arVega@gmail.com","3885523329");
 			
-			Carrera carrera1 = new Carrera(1, "Ing. Informatica", (byte)5, "Activa");
-			Carrera carrera2 = new Carrera(1, "Profesorado de Ingles", (byte)4, "Activa");
-			Carrera carrera3 = new Carrera(1, "Analista Programador Universitario", (byte)3, "Activa");
-			
-			materias.add(new Materia("A11","Programacion Visual", "12", 6, "Virtual",docente3,carrera3));
-			materias.add(new Materia("A22","Ingles Avanzado", "1", 4, "Presencial",docente2,carrera2));
-			materias.add(new Materia("A33","Estructura de datos", "10", 6, "Virtual",docente1,carrera1));
+			materias.add(new Materia("A11","Programacion Visual", "12", 6, "Virtual",collectionDocente.getDocente("1"),collectionCarrera.getCarrera(3)));
+			materias.add(new Materia("A22","Ingles Avanzado", "1", 4, "Presencial",collectionDocente.getDocente("2"),collectionCarrera.getCarrera(1)));
+			materias.add(new Materia("A33","Estructura de datos", "10", 6, "Virtual",collectionDocente.getDocente("3"),collectionCarrera.getCarrera(2)));
 		}
 		return materias;
 	}
