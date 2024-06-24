3888950e3cf93f1a04746f2aecf64ce84c101c84
AlvaroR15 <alvaroramos048@gmail.com>
2024-06-23 23:37:46 -0300

    Metodos implementados para el CRUD de carrera

--- src/main/java/ar/edu/unju/fi/service/CarreraService.java
+++ src/main/java/ar/edu/unju/fi/service/CarreraService.java
@@ -1,7 +1,14 @@
 package ar.edu.unju.fi.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import java.util.List;
+
+
+import ar.edu.unju.fi.model.Carrera;
 
 public interface CarreraService {
-		
+		public List<Carrera> getCarreras();
+		public Carrera findById(Integer id);
+		public void saveCarrera(Carrera carrera);
+		public void editCarrera(Carrera carrera, Integer id);
+		public void deleteCarrera(Integer id);
 }
