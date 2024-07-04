dbbb90615c49835579e6f4e866e89964f6d08d4e
Alexis <alexisvilte9@gmail.com>
2024-07-04 00:18:20 -0300

    Modelo alumno refactorizado con anotaciones lombok.

--- src/main/java/ar/edu/unju/fi/model/Alumno.java
+++ src/main/java/ar/edu/unju/fi/model/Alumno.java
@@ -1,11 +1,16 @@
 package ar.edu.unju.fi.model;
 
-
 import java.time.LocalDate;
-
 import org.springframework.stereotype.Component;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Component
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
 public class Alumno {
 	private String dni;
 	private String nombre;
@@ -14,100 +19,6 @@ public class Alumno {
 	private String telefono;
 	private LocalDate fechaNacimiento;
 	private String domicilio;
-	private String lu;
-	
-	public Alumno() {
-		// TODO Auto-generated constructor stub
-	}
-	
-	
-
-	public Alumno(String dni, String nombre, String apellido, String email, String telefono, LocalDate fechaNacimiento,
-			String domicilio, String lu) {
-		this.dni = dni;
-		this.nombre = nombre;
-		this.apellido = apellido;
-		this.email = email;
-		this.telefono = telefono;
-		this.fechaNacimiento = fechaNacimiento;
-		this.domicilio = domicilio;
-		this.lu = lu;
-	}
-
-
-
-	public String getDni() {
-		return dni;
-	}
-
-	public void setDni(String dni) {
-		this.dni = dni;
-	}
-
-	public String getNombre() {
-		return nombre;
-	}
-
-	public void setNombre(String nombre) {
-		this.nombre = nombre;
-	}
-
-	public String getApellido() {
-		return apellido;
-	}
-
-	public void setApellido(String apellido) {
-		this.apellido = apellido;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-	public String getTelefono() {
-		return telefono;
-	}
-
-	public void setTelefono(String telefono) {
-		this.telefono = telefono;
-	}
-
-	public LocalDate getFechaNacimiento() {
-		return fechaNacimiento;
-	}
-
-	public void setFechaNacimiento(LocalDate fechaNacimiento) {
-		this.fechaNacimiento = fechaNacimiento;
-	}
-
-	public String getDomicilio() {
-		return domicilio;
-	}
-
-	public void setDomicilio(String domicilio) {
-		this.domicilio = domicilio;
-	}
-
-	public String getLu() {
-		return lu;
-	}
-
-	public void setLu(String lu) {
-		this.lu = lu;
-	}
-
-
-
-	@Override
-	public String toString() {
-		return "Alumno [dni=" + dni + ", nombre=" + nombre + ", apellido=" + apellido + ", email=" + email
-				+ ", telefono=" + telefono + ", fechaNacimiento=" + fechaNacimiento + ", domicilio=" + domicilio
-				+ ", lu=" + lu + "]";
-	}
-	
-	
+	private String lu; //
 }
+	
